4c2f1b3d71b1017fa3fad372c4bde091a3ab5ef8
David Furlong <dvfurlong@googlemail.com>
2025-07-11 11:45:06 +0200

    update walletconnect project id

--- app/config.ts
+++ app/config.ts
@@ -3,7 +3,7 @@ import { base } from "wagmi/chains";
 
 export const config = getDefaultConfig({
   appName: "ECP Channel tool",
-  projectId: "97a1ef18f472442c33d8858a9a5c40bd", // Get from https://cloud.walletconnect.com
+  projectId: "ed569a4fc1905c6af281d2789624c345", // Get from https://cloud.walletconnect.com
   chains: [base],
   ssr: false,
 });
